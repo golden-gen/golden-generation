c4534ab5e7837a9de515c786f0f388cf39324fc4
jocanola <jokanola.it@gmail.com>
2024-12-03 18:42:21 +0100

    fix: change landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import ComingSoon from "./coming-soon";
 import LandingPage from "./landing-page";
+import RegisterPage from "./student";
 
 export default function Home() {
   // firebase deploy --only hosting:golden-generation
-  return <ComingSoon />;
+  return <RegisterPage />;
 }
